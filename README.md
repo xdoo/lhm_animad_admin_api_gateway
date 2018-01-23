af8eb759859d76c587aba65931170d2c6d8f2afb
rowe42 <rolandwrn@gmail.com>
2018-01-23 16:27:15 +0100

    Version mit Spring Cloud Gateway und Netty

--- README.md
+++
@@ -1,2 +0,0 @@
-# lhm_api_gateway
-the zuul api-gateway
--- src/main/java/de/muenchen/referenzarchitektur/apigateway/ApiGatewayApplication.java
+++ src/main/java/de/muenchen/referenzarchitektur/apigateway/ApiGatewayApplication.java
@@ -2,17 +2,11 @@ package de.muenchen.referenzarchitektur.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-@EnableZuulProxy
-@EnableOAuth2Sso
-public class ApiGatewayApplication extends WebSecurityConfigurerAdapter {
+public class ApiGatewayApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
 }
