69c8cc53bfe295ca6f814a6cd4c4af0a0935dec7
rowe42 <rolandwrn@gmail.com>
2018-01-30 09:56:44 +0100

    HAL-Support und RootController ergänzt, um Root-URL zu bekommen; Offen: sobald html5 dabei ist, wird / zu index.html weitergereicht. Wie verhindere ich das?

--- src/main/java/de/muenchen/referenzarchitektur/apigateway/ApiGatewayApplication.java
+++ src/main/java/de/muenchen/referenzarchitektur/apigateway/ApiGatewayApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableOAuth2Sso
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 public class ApiGatewayApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
