41f9f6fbcd2cc0055d6c06f867a7982a07cc5a5b
rowe42 <rolandwrn@gmail.com>
2018-02-09 15:49:45 +0100

    CSRF-Token disabled, Backend-Pfad auf /api/ gesetzt

--- src/main/java/de/muenchen/referenzarchitektur/apigateway/ApiGatewayApplication.java
+++ src/main/java/de/muenchen/referenzarchitektur/apigateway/ApiGatewayApplication.java
@@ -2,10 +2,8 @@ package de.muenchen.referenzarchitektur.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableZuulProxy
